a0fb67b04dcf09033efdbc39fcf368e5d1ea74ea
rahulunair@gmail.com <rahulunair@gmail.com>
2020-06-20 14:10:22 -0700

    check for repo done.

--- src/shapi/client.py
+++ src/shapi/client.py
@@ -1,17 +1,19 @@
 """client bindings to query software heritage API."""
+from functools import lru_cache
 import logging
 import os
 import sys
-from urllib.parse import urlencode
 
 import requests
+from requests.utils import quote
+
 logger = logging.getLogger("SHClient")
 logging.basicConfig(level=os.environ.get("LOGLEVEL", "INFO"))
 
-
 class SHClient:
+    
     URL = "https://archive.softwareheritage.org/api/1/"
-
+    
     def __init__(self, rate_limit=None):
         self.rate_limit = rate_limit
         self.session = self._init_session()
@@ -20,29 +22,29 @@ class SHClient:
         return requests.Session()
 
     def _form_url(self, path: str) -> str:
-        return self.URL + path
+        return self.URL + quote(path)
 
     def _handle_response(self, response):
-        print(response.status_code)
         if response.status_code == 404:
-            logger.info("API call triggered a bad request")
+            logger.error("API call triggered a bad request")
             sys.exit(1)
-        return response
+        elif response.status_code == 301:
+            logger.error("API URL moved, check the URL")
+            sys.exit(1)
+        else:
+            return response
 
     def _send_request(
         self, method: str, path: str, params: str = None, data=None, headers=None
     ):
         url = self._form_url(path)
-        logger.info(url)
         req = self.session.request(
-            method=method,
-            url=url,
-            params=params,
-            data=data,
-            headers=headers
+            method=method, url=url, params=params, data=data, headers=headers
         )
         return req
-
+    
+    @lru_cache()
     def query(self, method, path, params=None, data=None, headers=None):
         req = self._send_request(method, path)
-        return self._handle_response(req)
+        resp = self._handle_response(req)
+        return resp.json()
--- src/shapi/console.py
+++ src/shapi/console.py
@@ -1,32 +1,43 @@
 """CLI entrypoint to shapi."""
+import json
+
 import click
 
 from . import __version__
+from .client import SHClient
 
-from  client import SHClient
-
+sh_client = SHClient()
 
 @click.group()
 @click.version_option(version=__version__)
 def cli() -> None:
-    """SHApi - Software Heritage API.
+    """SHApi - Software Heritage(SH) API.
 
     A Python client to interact with software heritage API
     to query for an object, upload new pkgs to the repo.
     """
 
 
-@cli.command("check")
-@cli.argument("hash", type=str)
-def check(hash: str) -> None:
-    """Check if a package is available using its hash.
+def _origin_check(url: str) -> str:
+    return "origin/{}/visit/latest/".format(url)
 
-    The hash can be sha1 or sha2."""
+
+@cli.command("check")
+@click.argument("url", type=str)
+def check(url: str) -> None:
+    """Check if a repo url is archived in SH.
+
+    Give the full URL of the repo hosted in git, svn etc.
+
+    Args:
+        url: repo url to check, eg: https://github.com/tensorflow/tensorflow
     """
-    res: dict = client.check(hash)
-    if res["available"]:
-        print("pkg in Software Heritage,")
-        print("src url: {}".format(res["url"]))
-    else:
-        print("couldn't fine the package in SH, consider upload it.")
-    """
+    url = _origin_check(url)
+    #TODO (unrahul): cache
+    resp = sh_client.query("GET", url)
+    print("=" * 71)
+    print("Archive found")
+    print("Date: {}".format(resp["date"]))
+    print("Src URL: {}".format(resp["origin"]))
+    print("Archive URL: {}".format(resp["snapshot_url"]))
+    print("=" * 71)
